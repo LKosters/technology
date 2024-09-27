330ddd13b3b5bd517e3e52a6336a7fac71c02df7
LKosters <lau.kosters@gmail.com>
2024-09-27 10:59:59 +0200

    Challenge 9.1.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,55 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int led1 = 4;
+const int led2 = 5;
+const int led3 = 6;
+
+const int buttonPin = 8;
+
+int currentLed = 1;
+
+int lastButtonState = HIGH;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  digitalWrite(led1, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  int currentButtonState = digitalRead(buttonPin);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (currentButtonState != lastButtonState) {
+    if (currentButtonState == LOW && (millis() - lastDebounceTime > debounceDelay)) {
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+
+      if (currentLed == 1) {
+        currentLed = 2;
+      } else if (currentLed == 2) {
+        currentLed = 3;
+      } else {
+        currentLed = 1;
+      }
+
+      if (currentLed == 1) {
+        digitalWrite(led1, HIGH);
+      } else if (currentLed == 2) {
+        digitalWrite(led2, HIGH);
+      } else if (currentLed == 3) {
+        digitalWrite(led3, HIGH);
+      }
+
+      lastDebounceTime = millis();
+    }
+  }
+
+  lastButtonState = currentButtonState;
 }
