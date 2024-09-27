afca635b08f4f7e177230c001c93559d8e8182a7
LKosters <lau.kosters@gmail.com>
2024-09-27 09:43:42 +0200

    Challenge 6.1.5

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(4, OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  digitalWrite(4, HIGH);
+  
+  Serial.print("LED ON ");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(1000);
+  
+  digitalWrite(4, LOW);
+  
+  Serial.print("LED OFF ");
+
+  delay(1000);
 }
