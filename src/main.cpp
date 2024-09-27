6e8e07433fadda41b19570b4cef2f7ddee890cf2
LKosters <lau.kosters@gmail.com>
2024-09-27 10:13:31 +0200

    Challenge 7.1.4

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  
+  // int
+  int intValue = 42; 
+  Serial.print("Int value: ");
+  Serial.println(intValue, BIN);
+  
+  // unsigned int
+  unsigned int uintValue = 42; 
+  Serial.print("Unsigned int value: ");
+  Serial.println(uintValue, BIN);
+  
+  // long
+  long longValue = 100000L; 
+  Serial.print("Long value: ");
+  Serial.println(longValue, BIN);
+  
+  // unsigned long
+  unsigned long ulongValue = 100000UL;
+  Serial.print("Unsigned long value: ");
+  Serial.println(ulongValue, BIN);
+  
+  // char
+  char charValue = 'A';
+  Serial.print("Char value: ");
+  Serial.println(charValue, BIN);
+  
+  // boolean
+  boolean boolValue = true; 
+  Serial.print("Boolean value: ");
+  Serial.println(boolValue, BIN);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
