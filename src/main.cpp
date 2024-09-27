4785db3653c1132d08d02d3d730e9d6fccca0523
LKosters <lau.kosters@gmail.com>
2024-09-27 10:05:58 +0200

    Challenge 7.1.3

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
+  // char
+  Serial.print("char: ");
+  char charMin = -128;
+  char charMax = 127;
+  Serial.print(charMin);
+  Serial.print(" to ");
+  Serial.println(charMax);
+
+  // int
+  Serial.println("Testing overflow...");
+  int intOverflow = 32767;
+  Serial.print("int before overflow: ");
+  Serial.println(intOverflow);
+  intOverflow++;
+  Serial.print("int after overflow: ");
+  Serial.println(intOverflow);
+
+  // unsigned int
+  unsigned int uintOverflow = 65535;
+  Serial.print("unsigned int before overflow: ");
+  Serial.println(uintOverflow);
+  uintOverflow++;
+  Serial.print("unsigned int after overflow: ");
+  Serial.println(uintOverflow);
+
+  // long
+  long longOverflow = 2147483647;
+  Serial.print("long before overflow: ");
+  Serial.println(longOverflow);
+  longOverflow++;
+  Serial.print("long after overflow: ");
+  Serial.println(longOverflow);
+
+  // unsigned long
+  unsigned long ulongOverflow = 4294967295;
+  Serial.print("unsigned long before overflow: ");
+  Serial.println(ulongOverflow);
+  ulongOverflow++;
+  Serial.print("unsigned long after overflow: ");
+  Serial.println(ulongOverflow);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
