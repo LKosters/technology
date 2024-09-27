f8197469a6c2ab6be122e474741a53f2e8246fdc
LKosters <lau.kosters@gmail.com>
2024-09-27 09:46:53 +0200

    Challenge 6.1.6

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(4, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  digitalWrite(4, HIGH);
+  
+  delay(1000);
+  
+  digitalWrite(4, LOW);
+  
+  delay(1000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(5, HIGH);
+  
+  delay(1000);
+  
+  digitalWrite(5, LOW);
+
+  delay(1000);  
 }
