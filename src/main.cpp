6175f7db9813f5fc48249826cb075ef939cfdd0b
LKosters <lau.kosters@gmail.com>
2024-09-27 09:28:51 +0200

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include <Arduino.h>
 
 void setup() {
-  pinMode(4, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(4, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
   
   delay(1000);
   
-  digitalWrite(4, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   
   delay(1000);
 }
